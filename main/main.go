3e3141a16cb1fc0651796b1a4ba1263eeb33ad1c
tomatome <362521329@qq.com>
2018-04-20 16:49:25 +0800

    page

--- main/main.go
+++ main/main.go
@@ -56,6 +56,7 @@ func server() *socketio.Server {
 					hasFound = true
 					room = c.room
 					log.Println("join in room: ", c.room)
+					so.BroadcastTo(room, "chat message", addr+" join in room")
 					break
 				}
 			}
